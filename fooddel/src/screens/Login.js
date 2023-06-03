f31b8113dab98daefeeb23bff6016de01116614f
dhruvilun522 <103945956+dhruvilun522@users.noreply.github.com>
2023-06-03 13:10:37 +0530

    Update Login.js

--- fooddel/src/screens/Login.js
+++ fooddel/src/screens/Login.js
@@ -8,7 +8,7 @@ export default function Login() {
     const handleSubmit= async(e)=>{ 
 
         e.preventDefault();
-        const response= await fetch("http://localhost:5000/api/loginuser",{
+        const response= await fetch("https://mealmate-ws3d.onrender.com/api/loginuser",{
             method:'POST',
             headers:{
                 'Content-Type':'application/json'
