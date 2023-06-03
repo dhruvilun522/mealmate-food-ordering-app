086c4c9363be0da2baf5a8c7f04e9a10ee6a22c2
dhruvilun522 <103945956+dhruvilun522@users.noreply.github.com>
2023-06-03 13:11:10 +0530

    Update Signup.js

--- fooddel/src/screens/Signup.js
+++ fooddel/src/screens/Signup.js
@@ -9,7 +9,7 @@ export default function Signup() {
     const handleSubmit= async(e)=>{ 
 
         e.preventDefault();
-        const response= await fetch("http://localhost:5000/api/createuser",{
+        const response= await fetch("https://mealmate-ws3d.onrender.com/api/createuser",{
             method:'POST',
             headers:{
                 'Content-Type':'application/json'
