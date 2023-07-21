600efc571f3dc1c5eddd477f408be9b84ad950f7
dhruvilun522 <103945956+dhruvilun522@users.noreply.github.com>
2023-07-21 16:59:19 +0530

    Update index.js

--- fooddel/backend/index.js
+++ fooddel/backend/index.js
@@ -8,7 +8,7 @@ app.get('/', (req, res) => {
 })
 
 app.use((req,res,next)=>{
-  res.setHeader('Access-Control-Allow-Origin','*');
+  res.setHeader('Access-Control-Allow-Origin',*);
   res.header(
     'Access-Control-Allow-Headers',
     'Origin, X-Requested-With, Content-Type , Accept'
